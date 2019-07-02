45e380de9efd5ff03ef3abac451517648a28b1f1
Jack Birch <jak.r.birch@gmail.com>
2019-07-02 14:29:17 +0100

    This text file contains information on how  to use tuples

---
+++ tuples.py
@@ -0,0 +1,6 @@
+T=(1,2,3,4) 		#A tuple is a immutable list - you can't change it
+print(len(T))
+print(T)
+
+T=([1,2],3,4) 		#Support nesting but dont grow and shrink because they are immutable 
+print(T)
