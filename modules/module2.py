d3619082c934b57ab4f6763e5b20f95622a28b13
Jack Birch <jak.r.birch@gmail.com>
2019-07-11 15:44:12 +0100

    A few program files that have been added on modules - Still need to work out how to use path files.

---
+++ modules/module2.py
@@ -0,0 +1,6 @@
+import module1
+module1.spam('gumby')       #This will output 'gumby spam'
+
+#This imports a module turns it into byte code store the module object in memory 
+#The code can then run the module object whenever it needs to 
+#The module1.spam corresponds to module.attribute
